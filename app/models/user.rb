7cb7144cf583de21b123cddcaa83e61d1aebd045
irvingvillasenor <irving@pipersolution.com>
2020-08-06 00:54:03 -0500

    updates seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   validates :first_name, length: {minimum: 2}, presence: true
   validates :last_name, length: {minimum: 2}, presence: true
   validates :email, presence: true, uniqueness: true
-  validates :image_url, presence: true
+  # validates :image_url, presence: true
 
 
   has_many :vendors, dependent: :destroy
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,44 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # Categories
+users = User.create!([
+  {
+    first_name: "Irving",
+    last_name: "Villasenor",
+    email: "villasenor14@gmail.com",
+    password: "password",
+    password_confirmation: "password",
+    image_url: ""
+  },
+  {
+    first_name: "Joann",
+    last_name: "Villasenor",
+    email: "joann@gmail.com",
+    password: "password",
+    password_confirmation: "password",
+    image_url: ""
+  },
+  {
+    first_name: "Noel",
+    last_name: "Villasenor",
+    email: "noel@gmail.com",
+    password: "password",
+    password_confirmation: "password",
+    image_url: ""
+  },
+  {
+    first_name: "Fannie",
+    last_name: "Villasenor",
+    email: "fannie@gmail.com",
+    password: "password",
+    password_confirmation: "password",
+    image_url: ""
+  }
+])
 
 categories = Category.create!([
+
+  
   {
     name: "Rental Equipment",
     image_url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.platinumeventrentals.com%2F&psig=AOvVaw2miQkzAEfblwOAIKDlQAnp&ust=1596763126407000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLDLsam0hesCFQAAAAAdAAAAABAE"
@@ -54,7 +90,7 @@ categories = Category.create!([
 #   {
 #     budget: 35000,
 #     occasion: "Wedding",
-#     user_id: 2
+#     user_id: 1
 #   },
 #   {
 #     budget: 5000,
@@ -76,7 +112,7 @@ vendors = Vendor.create!([
     category_id: 1,
     zip_code: "3900 W 74th St.
     Chicago, IL 60629",
-    user_id: 2,
+    user_id: 1,
     website_url: "www.chicagomoonwalks.com",
     image_url: "https://lh3.googleusercontent.com/proxy/ZvHlAYBevqED3pyLGF7-XmSV96pOhULqdCGRmjpbW5gFKofz5pHsGct_V-RcdlMnf3k5Y7He5BYdQ_W9-zTioxVcWYM5i303GQQHQCuoI7wvmEq8U1_ARM3AmmLo4xa1_fVeXifI0pTVtiGiZ48t9lc",
     details: "Thank you for visiting ChicagoMoonwalks.com; where you know you can always find the best inflatable fun in the Chicago area. We Deliver throughout the Chicagoland & Suburbs, and our units are the safest for your children, All are roofed & include safety step, unless indicated otherwise.
@@ -88,7 +124,7 @@ vendors = Vendor.create!([
     category_id: 1,
     zip_code: "255-257 Commonwealth Dr.
     Carol Stream, IL 60188",
-    user_id: 2,
+    user_id: 1,
     website_url: "www.thefunones.com",
     image_url: "https://zncsolutions.com/wp-content/uploads/2016/12/IMG_20391.jpg",
     details: "If you need a table rental for your next event in Chicago, IL, or any of the surrounding suburbs, turn to the company that thousands of area residents have trusted for their party rentals – The Fun Ones. As a full-service event rental company, we provide a wide array of rentals that are available in any size order and a variety of different styles.",
@@ -99,7 +135,7 @@ vendors = Vendor.create!([
     category_id: 1,
     zip_code: "650 N. Raddant Rd.
     Batavia, IL 60510",
-    user_id: 2,
+    user_id: 1,
     website_url: "www.bluepeaktents.com",
     image_url: "https://bluepeaktents.com/wp-content/uploads/2018/10/IMG_4877-600x400.jpg",
     details: "Blue Peak Tents is a quality rental provider of tent rentals, party rentals, wedding tents, and event products for the greater Chicagoland area. A proven track record of quality service and products has made Blue Peak Tents the benchmark for tent rentals and quality events. Blue Peak follows a motto of “Complete Customer Service.",
@@ -110,7 +146,7 @@ vendors = Vendor.create!([
     category_id: 1,
     zip_code: "626 Oakview Ave
     Joliet, IL 60433",
-    user_id: 2,
+    user_id: 1,
     website_url: "www.joliettent.com",
     image_url: "https://tentandtable.net/media/wordpress/b07b031be26f8fd646ca4c39f0655f31.jpg",
     details: "Customized packages for your wedding needs. Services include delivery, setup & cleanup. Wedding tent rentals, linens, chairs, lighting, heating, air-conditioning, and more. Tents and Event Rentals. High Peak & Regular Frame. Dance Floors and Stages.",
@@ -121,7 +157,7 @@ vendors = Vendor.create!([
     category_id: 1,
     zip_code: "5819 S Madison St.
     Hinsdale, IL 60521",
-    user_id: 2,
+    user_id: 1,
     website_url: "www.chicagotentrental.com",
     image_url: "https://lh3.googleusercontent.com/proxy/7J5_OPjkarVopCZ_pJ5NGBXNTYD7L6O8EReTMGswvphjFZz3tLM9apj9BfE1hD789gaRfRYGKzY-TLxuehh_u3KKUCBR47EX_DD_eym97qpEnbcqg_bwZwhh1sKMhyVYnXtQNNffmg",
     details: "Tents mean business and we've been working in the corporate sector for years providing tents for a variety of functions all housed under the big top.We provide free consultations for tent installations at
@@ -133,7 +169,7 @@ vendors = Vendor.create!([
     category_id: 1,
     zip_code: "9480 W 55th St
     McCook IL 60525",
-    user_id: 2,
+    user_id: 1,
     website_url: "www.marqueerents.com",
     image_url: "https://i.pinimg.com/originals/ed/55/1b/ed551b85cf8d72d52d364882886c3d3e.jpg",
     details: "We offer all the products you need for your event. From bars and buffets, to tables and tents, we have the largest inventory in the industry. Contact our expert event specialists for a complimentary consultation.",
@@ -145,7 +181,7 @@ vendors = Vendor.create!([
     category_id: 4,
     zip_code: "3118 N. Lincoln Avenue
     Chicago, IL 60657",
-    user_id: 2,
+    user_id: 1,
     website_url: "www.ashaddflorist.com",
     image_url: "https://www.floraqueen.com/blog/wp-content/uploads/2020/03/shutterstock_1597240777.jpg",
     details: "Ashland Addison is a family owned and operated florist, established in 1932 at the corner of Ashland and Addison in Chicago. It started when our grandfather, S. Roy Sheffield, was paid with flowers instead of cash by local flower growers for carting flowers to market.",
@@ -156,7 +192,7 @@ vendors = Vendor.create!([
     category_id: 4,
     zip_code: "541 N Fairbanks Ct.
     Chicago, IL 60611",
-    user_id: 2,
+    user_id: 1,
     website_url: "www.chicagosmarcelflorist.com",
     image_url: "https://www.floraqueen.com/blog/wp-content/uploads/2020/03/shutterstock_1597240777.jpg",
     details: "Chicago Flower Is Your Local Downtown Chicago Florist.  Send Farm Fresh Flowers To Chicago From Anywhere In The World! 100,000 Flower Arrangements Delivered To Happy Recipients In The Chicago Area.  Our Clients Include U.S. Presidents, (Current & Past) Members of the Political Ruling Class, (From Both Sides Of The Aisle), Foreign Heads Of State, State Political Figures, National and Local News Persons, Forbes Top 100 Wealthy, Hollywood Stars and Starlets, Rock Stars and Rock Legends, and so many more.",
@@ -167,7 +203,7 @@ vendors = Vendor.create!([
     category_id: 4,
     zip_code: "407 East 71 St
     Chicago IL 60619",
-    user_id: 2,
+    user_id: 1,
     website_url: "www.southchicagoflorist.com",
     image_url: "https://img.theculturetrip.com/768x432/wp-content/uploads/2018/02/florist.jpg",
     details: "Leo's Metropolitan Florist has been hand-delivering premium floral arrangements and gift baskets to help strengthen relationships, give love and support, and celebrate life’s special moments since 1967. From birthdays to anniversaries to commemorating a loved one, and every little “just because” moment in between.",
@@ -178,7 +214,7 @@ vendors = Vendor.create!([
     category_id: 4,
     zip_code: "541 N Fairbanks Ct.
     Chicago, IL 60611",
-    user_id: 2,
+    user_id: 1,
     website_url: "www.chicagosmarcelflorist.com",
     image_url: "https://images.shopflowers.net/images/Vendors/Aboutus_144840_7402.jpg",
     details: "LaSalle Flowers has been a family owned florist on the corner of LaSalle and Superior since 1936. Today, LaSalle Flowers has continues the tradition of providing friendly customer service, beautiful fresh cut flowers, a wide selection of green and blooming plants, balloons and top quality plush animals. We also specialize in Weddings, Corporate parties and Corporate Weeklys.",
@@ -189,7 +225,7 @@ vendors = Vendor.create!([
     category_id: 4,
     zip_code: "5862 W. Higgins Ave. 
     Chicago, IL  60630",
-    user_id: 2,
+    user_id: 1,
     website_url: "www.southchicagoflorist.com",
     image_url: "https://img.theculturetrip.com/768x432/wp-content/uploads/2018/02/florist.jpg",
     details: "For the best flower arrangements and gifts in Chicago, IL visit Wall's Flower Shop, Inc.. Our professional florists can help you find the perfect flowers for any individual or occasion. Wall's Flower Shop, Inc. delivers flower arrangements and custom bouquets throughout Chicago and offers same-day flower delivery for last-minute gift needs! We also offer nationwide flower delivery through our trusted florist network.",
@@ -200,7 +236,7 @@ vendors = Vendor.create!([
     category_id: 4,
     zip_code: "1415 W. Morse Ave.
     Chicago, IL 60626",
-    user_id: 2,
+    user_id: 1,
     website_url: "www.rogersparkflorist.com",
     image_url: "https://images.shopflowers.net/images/Vendors/Aboutus_144840_7402.jpg",
     details: "We are committed to offering only the finest floral arrangements and gifts, backed by service that is friendly and prompt. Because all of our customers are important, our professional staff is dedicated to making your experience a pleasant one. That is why we always go the extra mile to make your floral gift perfect. Let Rogers Park Florist be your first choice for flowers.",
@@ -212,7 +248,7 @@ vendors = Vendor.create!([
     category_id: 2,
     zip_code: "1821 W. Hubbard St. 
     Chicago, IL 60612",
-    user_id: 4,
+    user_id: 2,
     website_url: "www.fultonstreetcollective.com",
     image_url: "https://www.fultonstreetcollective.com/uploads/5/2/5/6/5256133/gallery-chicago-art-fulton-street_2.jpg",
     details: "Founded in 2002 by singer-songwriters Anna Fermin and Joe Lanasa, the Fulton Street Collective grew out of a small rehearsal room in the corner of a print-shop business. Today FSC features 28 private artists studios and a beautiful gallery exhibition/event space.",
@@ -223,7 +259,7 @@ vendors = Vendor.create!([
     category_id: 2,
     zip_code: "433 W Van Buren St.
     Chicago, IL 60607",
-    user_id: 4,
+    user_id: 2,
     website_url: "www.theknot.com",
     image_url: "https://media-api.xogrp.com/images/6d89b688-bbd9-4cc9-a14e-ef64f6cd575a~rs_720.480",
     details: "Chicago’s newest venue comes to life at The Old Post Office. Your guests will love the two-story Historic Lobby, timeless with its 1920’s tributes of white marble and gold accents. Paired with our unique, modern reception rooms and east-facing views of the Chicago River, this historic landmark provides the perfect backdrop for your wedding.",
@@ -234,7 +270,7 @@ vendors = Vendor.create!([
     category_id: 2,
     zip_code: "2110 S Wabash Ave
     Chicago, IL 60616",
-    user_id: 4,
+    user_id: 2,
     website_url: "www.chicagoilluminatingcompany.com",
     image_url: "https://media-api.xogrp.com/images/f2591cf7-679f-4bca-8ac2-4c8c5fb1a017~rs_720.480",
     details: "The Chicago Illuminating Compant, located at 2110 S Wabash Ave in Chicago's hip South Loop neighborhood, is Chicago's premier events space.",
@@ -245,7 +281,7 @@ vendors = Vendor.create!([
     category_id: 2,
     zip_code: "140 East Walton Place
     Chicago, IL 60611",
-    user_id: 4,
+    user_id: 2,
     website_url: "www.thedrakehotel.com",
     image_url: "https://d2zy10gslnidpn.cloudfront.net/uploads/pages/page_asset/image/56/large_PalmCourt.jpg",
     details: "For those who love their afternoon teas fresh, colorful, fragrant and steeped in history, Palm Court at The Drake, a Hilton Hotel, serving tea for nearly a century in the Gold Coast neighborhood of Chicago, is the quintessential Magnificent Mile afternoon tea experience. Even the Queen and Princess Diana have made their royal visits here.",
@@ -256,7 +292,7 @@ vendors = Vendor.create!([
     category_id: 2,
     zip_code: "1356 Broadway at 36th Street
     New York, NY 10018",
-    user_id: 4,
+    user_id: 2,
     website_url: "www.gothamhallevents.com",
     image_url: "https://www.alux.com/wp-content/uploads/2014/09/Most-Expensive-Wedding-Venues-in-New-York2.jpg",
     details: "Located in a landmark building that once housed Greenwich Savings Bank, Gotham Hall is an iconic Midtown event space. Gotham Hall features a 9,000 square-foot Ballroom with 70-foot ceiling and an ornate stained-glass skylight.",
@@ -267,7 +303,7 @@ vendors = Vendor.create!([
     category_id: 2,
     zip_code: "255 S Canal St
     Chicago, IL 60606",
-    user_id: 4,
+    user_id: 2,
     website_url: "www.chicagounionstation.com",
     image_url: "https://d2zy10gslnidpn.cloudfront.net/uploads/pages/page_asset/image/56/large_PalmCourt.jpg",
     details: "Chicago Union Station features a series of stunning event spaces that will transport you and your guests to a special moment in our city’s story — one that is steeped in vintage glamour and bustling with excitement and energy. Hold your event at a one-of-a-kind venue. The lovingly restored event spaces of this Beaux-Arts station are perfect for any size and scale of event, from small corporate meetings to large galas and weddings.",
@@ -278,7 +314,7 @@ vendors = Vendor.create!([
     name: "Grupo Montez De Durnago",
     category_id: 2,
     zip_code: "60647",
-    user_id: 5,
+    user_id: 3,
     website_url: "www.grupomontezdedurango.com",
     image_url: "https://upload.wikimedia.org/wikipedia/commons/f/f2/Montez_De_Durango.jpg",
     details: "Regional Mexican band with over 18yrs of experience",
@@ -289,7 +325,7 @@ vendors = Vendor.create!([
     category_id: 2,
     zip_code: "3032 N. Southport Ave
     Chicago, IL 60657",
-    user_id: 5,
+    user_id: 3,
     website_url: "www.dj-chicago.com",
     image_url: "https://images2.minutemediacdn.com/image/upload/c_fill,g_auto,h_1248,w_2220/v1555324901/shape/mentalfloss/djs.jpg?itok=0CLhuJGv",
     details: "DJ-Chicago has set the standard for the DJ industry in Chicago for the past 15 years serving weddings, private parties, corporate events, and bars & clubs.",
@@ -299,7 +335,7 @@ vendors = Vendor.create!([
     name: "Bash Beats",
     category_id: 2,
     zip_code: "60647",
-    user_id: 5,
+    user_id: 3,
     website_url: "www.bashbeats.com",
     image_url: "https://media-api.xogrp.com/images/394e6f4b-84dd-4fdb-bfc8-8b4124a362e8~rs_719.480",
     details: "Bash Beats specializes in delivering high-quality sound, style, and energy to your weddings and events. With pricing customized for every event, we make sure you have everything you want for your special day without paying for anything you don't need.",
@@ -310,7 +346,7 @@ vendors = Vendor.create!([
     category_id: 2,
     zip_code: "6626 S Loomis Blvd.
     Chicago, IL 60636",
-    user_id: 5,
+    user_id: 3,
     website_url: "www.ddtheclown.com",
     image_url: "https://lh3.googleusercontent.com/proxy/IIT2mmIeI1JCWo3zVi7p9qY6II1ggc_BBMQ8WrNHqsSvE9W5vQbWF0eVSOfrb1hzPmS3sP_1-UbayXXbYyzeSX3AZYUS7xQdEg",
     details: "Ding Dong Chicago's Hip Hop Clown.",
@@ -321,7 +357,7 @@ vendors = Vendor.create!([
     category_id: 2,
     zip_code: "3712 N Kedvale Ave 
     Chicago, IL 60641",
-    user_id: 5,
+    user_id: 3,
     website_url: "www.chicagocatz.com",
     image_url: "https://media-api.xogrp.com/images/394e6f4b-84dd-4fdb-bfc8-8b4124a362e8~rs_719.480",
     details: "Albeit, R&B is his forte, Tony wears many, many hats. He plays R&B, Jazz, Rock and Gospel, and is the organist for Healing Temple C. O. G. I. C. He also plays drums, lead guitar and of course the bass. He has played for many talented artists, just to name a few: Aretha Franklin, B.B. King, Nancy Wilson,Etta James,Solomon Burke, James Brown.",
@@ -332,7 +368,7 @@ vendors = Vendor.create!([
     category_id: 2,
     zip_code: "155 N Upper Wacker Dr 
     Chicago, IL 60606",
-    user_id: 5,
+    user_id: 3,
     website_url: "www.ringmusicorchestra.com",
     image_url: "https://www.villazanchi.it/villa_per_ricevimenti/orchestra_big.jpg",
     details: "We are credible, reliable, and consistent in the quality we deliver. We are fun and pack dance floors, doing so with musical artistry and reasonable rates. Energy, passion, creativity, and a personal touch encompass the driving force behind our group. We combine the elements of logistics, music, and psychology to give you an experience unmatched by any other in the music industry.",
@@ -367,7 +403,7 @@ vendors = Vendor.create!([
     category_id: 7,
     zip_code: "15527 Keating Ave
     Oak Forest, IL 60452",
-    user_id: 6,
+    user_id: 4,
     website_url: "www.concinabyomar.com",
     image_url: "https://www.sanpellegrino.com/media//international/spyc2019/Sanpellegrino_SPYC_teaser_POST_01.jpg",
     details: "Private chef for dinner parties and events. Serving upscale dinner and adrinks at the comfort of your home.",
@@ -379,7 +415,7 @@ vendors = Vendor.create!([
     category_id: 3,
     zip_code: "2333 S Euclid Ave
     Berwyn, IL 60402",
-    user_id: 7,
+    user_id: 4,
     website_url: "www.chloecapture.com",
     image_url: "https://icdn2.digitaltrends.com/image/digitaltrends/dixie-dixon-interview-header.jpg",
     details: "Experienced special event photo/videographer in Chicagoland area",
@@ -391,7 +427,7 @@ vendors = Vendor.create!([
     category_id: 3,
     zip_code: "2448 n Southport Ave
     Chicago, IL 60614",
-    user_id: 8,
+    user_id: 4,
     website_url: "www.tintsalonspa.com",
     image_url: "http://www.tintsalonspa.com/images/gallery.png",
     details: "Experienced special event photo/videographer in Chicagoland area",
@@ -402,7 +438,7 @@ vendors = Vendor.create!([
     category_id: 3,
     zip_code: "1927 W Diversey Pkwy.
     Chicago, IL 60614",
-    user_id: 8,
+    user_id: 4,
     website_url: "www.theboardroom.com",
     image_url: "https://lh3.googleusercontent.com/proxy/NUsOhlefTCDBPK4ZUzZPP3Rsqel9rc3M1BBlka-zr5CmEm6XjtfKCCiHo_kRhMm6uFcd_iR8Ue-FRUqXoyKXwIYRpFWgr3lNjiiheymLbnqxPXs4uSeMxKpiq4LQ-6cid7Sg4q49Ab_D9Lei_Te_MsMGGWe2ptZOdyZh7DrfbIzie0hEtplMvv4xJuB-RRLue-urdoz5vwPzaCU8rwtkB7Nq5ls9TIBj",
     details: "Experienced special event photo/videographer in Chicagoland area",
